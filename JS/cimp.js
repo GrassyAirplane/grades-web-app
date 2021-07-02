fd68ff091d6478377ebc4896b0e8f433ef24670e
Euan <62548894+GrassyAirplane@users.noreply.github.com>
2021-07-02 19:59:19 +0800

    second commit

---
+++ JS/cimp.js
@@ -0,0 +1,43 @@
+//main dropdownButtons ID
+const dropdownButtonID= document.getElementById('dropdown-button-id');
+
+//dropDownList ID
+const dropdownListID = document.getElementById('dropdown-list-id');
+
+//dropdownButton SVG
+const dropdownSVG = document.getElementById('dropdown-button-svg');
+
+function participantNumber(){
+    //button id
+    const svg = document.getElementById('dropdown-button-svg');
+    const button = document.getElementById('dropdown-button-id');
+    const dropdownList = document.getElementById('dropdown-list-id');
+
+
+    //if the hover background is activated, deactivate else activate
+    if (button.style.backgroundColor == "rgba(0, 118, 255, 0.1)"){
+        button.style.backgroundColor = "transparent";
+        dropdownList.style.opacity = "0";
+        dropdownList.style.visibility = "hidden";
+        svg.style.transform = "rotate(0.5turn)";
+    }
+    else{
+        button.style.backgroundColor = "rgba(0, 118, 255, 0.1)"; 
+        dropdownList.style.opacity = "1";
+        dropdownList.style.visibility = "visible";
+        svg.style.transform = "rotate(0turn)";
+    }
+}
+
+function dropdownButton(clicked_id){
+    const btn = document.getElementById(clicked_id);
+    const svg = document.getElementById('dropdown-button-svg');
+    const button = document.getElementById('dropdown-button-id');
+    const dropdownList = document.getElementById('dropdown-list-id');
+
+    document.getElementById('participant-number').innerHTML = btn.textContent || btn.innerText;
+    dropdownList.style.opacity = "0";
+    dropdownList.style.visibility = "hidden";
+    svg.style.transform = "rotate(0.5turn)";
+    button.style.backgroundColor = "transparent";
+}
